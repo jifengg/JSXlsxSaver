3da9069b5c9bfa3c47712838852ae3f1e0e0dfa2
季风 <11-dongdong@163.com>
2019-05-13 12:26:03 +0800

    转移xml时，不需要转移空格

--- xlsxsaver.js
+++ xlsxsaver.js
@@ -502,7 +502,7 @@ ${numberFormatCount > 0 ? `<numFmts count="${numberFormatCount}">\n${numberForma
         s = str.replace(/&/g, "&amp;");
         s = s.replace(/</g, "&lt;");
         s = s.replace(/>/g, "&gt;");
-        s = s.replace(/ /g, "&nbsp;");
+        // s = s.replace(/ /g, "&nbsp;");
         s = s.replace(/\'/g, "&#39;");
         s = s.replace(/\"/g, "&quot;");
         return s;
